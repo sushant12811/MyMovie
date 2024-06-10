bab2adf4fa20c4b25b02fc9a6ce08281c93b4282
Nikunj Thakor <nikunjthakor2001@gmail.com>
2024-06-10 15:36:40 -0400

    Added LogOut Functinality

--- app/src/main/java/com/example/mymovie/MainActivity.java
+++ app/src/main/java/com/example/mymovie/MainActivity.java
@@ -6,7 +6,13 @@ import androidx.viewpager2.widget.ViewPager2;
 import android.content.Intent;
 import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
 
+import com.google.android.material.bottomnavigation.BottomNavigationView;
+import com.google.android.material.navigation.NavigationView;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
 
@@ -14,6 +20,7 @@ import java.util.Arrays;
 
 public class MainActivity extends AppCompatActivity {
 
+    private FirebaseAuth mAuth;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -21,7 +28,33 @@ public class MainActivity extends AppCompatActivity {
 
         setContentView(R.layout.activity_main);
 
+        BottomNavigationView bottomNavigationView = findViewById(R.id.bottomNavigationView);
+        bottomNavigationView.setOnNavigationItemSelectedListener(item -> {
+            int id = item.getItemId();
 
+            if (id == R.id.navigation_logout) {
+                performLogout();
+                return true;
+            }
+
+            // Handle other menu items if needed
+            // ...
+
+            return false;
+        });
+
+        mAuth = FirebaseAuth.getInstance();
+
+    }
+
+    private void performLogout() {
+        mAuth.signOut();
+
+        Toast.makeText(MainActivity.this, "Logged out successfully!", Toast.LENGTH_SHORT).show();
+
+        Intent intent = new Intent(MainActivity.this, LoginActivity.class);
+        startActivity(intent);
+        finish(); // Optional: Finish MainActivity
     }
 
 
