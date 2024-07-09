adf6fd3c8997da3e9958057e2ce5d6497e074130
Yash Patel <yashpatel@Yashs-MacBook.ht.home>
2024-07-08 22:54:09 -0400

    Added Setting Tab

---
+++ app/src/main/java/com/example/mymovie/SettingsActivity.java
@@ -0,0 +1,18 @@
+package com.example.mymovie;
+
+import android.os.Bundle;
+import androidx.appcompat.app.AppCompatActivity;
+
+public class SettingsActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_settings);
+
+        getSupportFragmentManager()
+                .beginTransaction()
+                .replace(R.id.settings_container, new SettingsFragment())
+                .commit();
+    }
+}
